3c6e7254b55abb978c887ffc8fcb74cad96364da
Andy Freeborough <andy@freeborough.com>
2016-01-15 00:36:19 +0000

    Added Categories module.
    This is the first part of the library.  It's not done using classes because static classes in Python are largely pointless, given how the import system works.

---
+++ Categories.py
@@ -0,0 +1,46 @@
+from Database import db
+from mysql.connector import Error
+
+def FetchById( id ):
+	"""
+	Fetches an individual category record from the database using the specified identifier.
+	
+	Args:
+		id: int, the unique identifier of the category.
+	
+	Returns:
+		A dict containing all the category fields: id, parent_id, name, code
+	"""
+	result = None
+	
+	try:
+		query = "SELECT * FROM categories WHERE id = %s;"
+		db.cursor.execute( query, ( id, ) )
+		result = db.cursor.fetchone()
+		
+	except Error as e:
+		print( e )	### TODO: Use a proper logging mechanism.
+	
+	return result
+
+
+def FetchByParentId( parent_id ):
+	"""
+	Fetch all the category records that have the specified parent_id.
+	
+	Args:
+		parent_id: int, the unique identifier of the parent category.
+	
+	Returns:
+		A list of dicts containing all the category records with the matching parent_id.
+	"""
+	result = None
+	
+	try:
+		query = "SELECT * FROM categories WHERE parent_id = %s;"
+		db.cursor.execute( query, ( parent_id, ) )
+		result = db.cursor.fetchall()
+	except Error as e:
+		print( e )	### TODO: Use a proper logging mechanism.
+	
+	return result
