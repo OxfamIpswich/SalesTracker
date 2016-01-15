a3f5af9284d859a6ca929d348789c65ca30184ea
Andy Freeborough <andy@freeborough.com>
2016-01-15 00:34:56 +0000

    Added basic database convenience class.
    This should make using a database connection throughout the library a little easier and less cumbersome.

---
+++ Database.py
@@ -0,0 +1,44 @@
+import mysql.connector
+from mysql.connector import Error
+
+# Datbase Connection/Cursor Configuration
+### TODO: Move these out to an ini file, and have them being server-specific settings.
+defaultConnectionParams = {
+	"host": "localhost",
+	"database": "salestracker",
+	"user": "oxfam",
+	"password": "f26080708"
+}
+
+defaultCursorParams = {
+	"dictionary": True
+}
+
+class Database:
+	"""
+	Convenience class for dealing with a common database connection.  You can just import the db variable into your
+	code and you'll have an open connection and cursor ready to use.
+	
+	Example:
+		
+		from Database import db
+		db.cursor.execute( "SELECT * FROM table_name;" )
+	"""
+	
+	connection = None
+	cursor = None
+	
+	def __init__( self, connectionParams, cursorParams ):
+		try:
+			self.connection = mysql.connector.connect( **connectionParams )
+			self.cursor = self.connection.cursor( **cursorParams )
+		except Error as e:
+			print( e )	### TODO: Use a proper logging mechanism.
+	
+	def __exit__( self ):
+		self.cursor.close()
+		self.connection.close()
+
+#	Create a default instance of a database connection which we can then just import without having to mess about with
+# connection parameters in every module.  If you want another just create your own instance of Database.
+db = Database( defaultConnectionParams, defaultCursorParams )
