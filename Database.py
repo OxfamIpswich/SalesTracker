66d31d17185ffe63df0f661dce24d66e16f21a79
connor youngs <youngsie97@gmail.com>
2016-01-15 21:27:19 +0000

    Renamed to Config.py and reimported it in the Database.py file

--- Database.py
+++ Database.py
@@ -1,18 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
-from python_mysql_dbconfig import read_db_config
+from Config import read_db_config
 
 # Datbase Connection/Cursor Configuration
 ### TODO: Move these out to an ini file, and have them being server-specific settings.
 defaultConnectionParams = read_db_config()
 
-
 defaultCursorParams = {
-	"dictionary": True
+    "dictionary": True
 }
 
+
 class Database:
-	"""
+    """
 	Convenience class for dealing with a common database connection.  You can just import the db variable into your
 	code and you'll have an open connection and cursor ready to use.
 	
@@ -21,21 +21,24 @@ class Database:
 		from Database import db
 		db.cursor.execute( "SELECT * FROM table_name;" )
 	"""
-	
-	connection = None
-	cursor = None
-	
-	def __init__( self, connectionParams, cursorParams ):
-		try:
-			self.connection = mysql.connector.connect( **connectionParams )
-			self.cursor = self.connection.cursor( **cursorParams )
-		except Error as e:
-			print( e )	### TODO: Use a proper logging mechanism.
-	
-	def __exit__( self ):
-		self.cursor.close()
-		self.connection.close()
 
-#	Create a default instance of a database connection which we can then just import without having to mess about with
+    connection = None
+    cursor = None
+
+    def __init__(self, connectionParams, cursorParams):
+        try:
+            self.connection = mysql.connector.connect(**connectionParams)
+            self.cursor = self.connection.cursor(**cursorParams)
+            
+
+        except Error as e:
+            print(e)  ### TODO: Use a proper logging mechanism.
+
+    def __exit__(self):
+        self.cursor.close()
+        self.connection.close()
+
+
+# Create a default instance of a database connection which we can then just import without having to mess about with
 # connection parameters in every module.  If you want another just create your own instance of Database.
-db = Database( defaultConnectionParams, defaultCursorParams )
+db = Database(defaultConnectionParams, defaultCursorParams)
