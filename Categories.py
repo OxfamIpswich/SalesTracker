75143c6b4295b4d9384c363367f3bfbaba94aea7
connor youngs <youngsie97@gmail.com>
2016-01-17 11:23:35 +0000

    Added logging to Categories.py Also tried to add a FetchAll method to it but cant get it to return anything other than None for some reason, feel free to delete it :P

--- Categories.py
+++ Categories.py
@@ -1,5 +1,6 @@
 from Database import db
 from mysql.connector import Error
+from Log import Log
 
 def FetchById( id ):
 	"""
@@ -12,15 +13,19 @@ def FetchById( id ):
 		A dict containing all the category fields: id, parent_id, name, code
 	"""
 	result = None
-	
+
 	try:
+		Log.info(('CATEGORIES-Fetch-Id:', 'Trying to grab data from table using Id'))
 		query = "SELECT * FROM category WHERE id = %s;"
 		db.cursor.execute( query, ( id, ) )
 		result = db.cursor.fetchone()
-		
+		Log.info(('CATEGORIES-Fetch-Id:', 'Successfully grabbed data'))
+
 	except Error as e:
-		print( e )	### TODO: Use a proper logging mechanism.
-	
+		Log.error(('CATEGORIES-Fetch-Id:', e))
+		Log.info(('CATEGORIES-Fetch-Id:Query:', query))
+		Log.info(('CATEGORIES-Fetch-Id:','Failed to grab data'))
+
 	return result
 
 
@@ -35,12 +40,36 @@ def FetchByParentId( parent_id ):
 		A list of dicts containing all the category records with the matching parent_id.
 	"""
 	result = None
-	
+
 	try:
+		Log.info(('CATEGORIES-Fetch-ParentId:', 'Trying to grab data from table using ParentId'))
 		query = "SELECT * FROM category WHERE parent_id = %s;"
 		db.cursor.execute( query, ( parent_id, ) )
 		result = db.cursor.fetchall()
+		Log.info(('CATEGORIES-Fetch-ParentId:', 'Successfully grabbed data'))
 	except Error as e:
-		print( e )	### TODO: Use a proper logging mechanism.
-	
+		Log.error(('CATEGORIES-Fetch-ParentId:', e))
+		Log.info(('CATEGORIES-Fetch-ParentId:Query:', query ))
+		Log.info(('CATEGORIES_Fetch-ParentId:', 'Failed to grab data'))
+
 	return result
+
+def FetchAll():
+
+
+	result = None
+
+	try:
+		Log.info(('CATEGORIES-Fetch-All:', 'Trying to grab all data from table'))
+		query = "SELECT * FROM category;"
+		db.cursor.execute( query )
+		result = db.cursor.fetchall()
+		Log.info(('CATEGORIES-Fetch-All', 'Successfully grabbed data'))
+	except Error as e:
+		Log.error(('CATEGORIES-Fetch-All', e))
+		Log.info(('CATEGORIES-Fetch-All:Query:', query))
+		Log.info(('CATEGORIES-Fetch-All:', 'Failed to grab data'))
+
+		return result
+
+
--- Log.py
+++ Log.py
@@ -5,7 +5,7 @@ loggingConfig = read_config(section='logging')
 loggingDict = {'NOTSET':0,'DEBUG':10,'INFO':20,'WARNING':30,'ERROR':40,'CRITICAL':50}
 logging.basicConfig(format='%(asctime)s %(name)-6s %(levelname)-8s %(message)s',**loggingConfig)
 
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+formatter = logging.Formatter('%(name)-2s: %(levelname)-2s %(message)s')
 console = logging.StreamHandler()
 console.setLevel(logging.ERROR)
 console.setFormatter(formatter)
